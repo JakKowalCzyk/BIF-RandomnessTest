af86f86d0873d2bcfdf8688eb0056984b0858b32
Kuba Kowalczyk <kubabrag12>
2019-05-13 14:37:29 +0200

    cleanup

--- bif.py
+++ bif.py
@@ -23,36 +23,40 @@ def lcg():
 
 count = 10000000
 
+def remove_file(file_name):
+    if os.path.exists(file_name):
+        os.remove(file_name)
 
 seedLCG(1)
 lcg_out_filename="lcg"
 mersenne_out_filename="mersenne"
 xoro_out_filename="xoroshiro"
 rg = RandomGenerator(Xoroshiro128(1234))
-os.remove("lcg.out")
-os.remove("mers.out")
-os.remove("xoro.out")
+remove_file("lcg.out")
+remove_file("mers.out")
+remove_file("xoro.out")
+
+
+def init_input_file(file):
+    file.write("type: d" + "\n")
+    file.write("count: " + str(count) + "\n")
+    file.write("numbit: 32" + "\n")
+
 
 with open(lcg_out_filename, mode="w") as lcg_file:
-    lcg_file.write("type: d" + "\n")
-    lcg_file.write("count: " + str(count) + "\n")
-    lcg_file.write("numbit: 32" + "\n")
+    init_input_file(lcg_file)
     for i in range(count):
         lcg_file.write(str(lcg()) + "\n")
     lcg_file.close()
 
 with open(mersenne_out_filename, mode="w") as mersenne_file:
-    mersenne_file.write("type: d" + "\n")
-    mersenne_file.write("count: " + str(count) + "\n")
-    mersenne_file.write("numbit: 32" + "\n")
+    init_input_file(mersenne_file)
     for i in range(count):
         mersenne_file.write(str(random.uniform(0, 10000000)) + "\n")
     mersenne_file.close()
 
 with open(xoro_out_filename, mode="w") as xoro_file:
-    xoro_file.write("type: d" + "\n")
-    xoro_file.write("count: " + str(count) + "\n")
-    xoro_file.write("numbit: 32" + "\n")
+    init_input_file(xoro_file)
     for i in range(count):
         xoro_file.write(str(rg.standard_normal()) + "\n")
     xoro_file.close()
@@ -64,33 +68,16 @@ x_log = open('xoro.out', 'a')
 m_log.flush()  # <-- h
 x_log.flush()  # <-- h
 
-#subprocess.Popen(["dieharder", "-d", "0", "-g", "202", "-f", lcg_out_filename], stdout=log_lcg, stderr=log_lcg)
-subprocess.Popen(["dieharder", "-d", "1", "-g", "202", "-f", lcg_out_filename], stdout=log_lcg, stderr=log_lcg)
-subprocess.Popen(["dieharder", "-d", "2", "-g", "202", "-f", lcg_out_filename], stdout=log_lcg, stderr=log_lcg)
 subprocess.Popen(["dieharder", "-d", "3", "-g", "202", "-f", lcg_out_filename], stdout=log_lcg, stderr=log_lcg)
-subprocess.Popen(["dieharder", "-d", "4", "-g", "202", "-f", lcg_out_filename], stdout=log_lcg, stderr=log_lcg)
-subprocess.Popen(["dieharder", "-d", "4", "-g", "202", "-f", lcg_out_filename], stdout=log_lcg, stderr=log_lcg)
-subprocess.Popen(["dieharder", "-d", "5", "-g", "202", "-f", lcg_out_filename], stdout=log_lcg, stderr=log_lcg)
-subprocess.Popen(["dieharder", "-d", "6", "-g", "202", "-f", lcg_out_filename], stdout=log_lcg, stderr=log_lcg)
-#subprocess.Popen(["dieharder", "-d", "7", "-g", "202", "-f", lcg_out_filename], stdout=log_lcg, stderr=log_lcg)
 subprocess.Popen(["dieharder", "-d", "14", "-g", "202", "-f", lcg_out_filename], stdout=log_lcg, stderr=log_lcg)
-#subprocess.Popen(["dieharder", "-a", "-g", "202", "-f", lcg_out_filename], stdout=log_lcg, stderr=log_lcg)
-#subprocess.Popen(["dieharder", "-d", "0", "-g", "202", "-f", mersenne_out_filename], stdout=m_log, stderr=m_log)
-subprocess.Popen(["dieharder", "-d", "1", "-g", "202", "-f", mersenne_out_filename], stdout=m_log, stderr=m_log)
-subprocess.Popen(["dieharder", "-d", "2", "-g", "202", "-f", mersenne_out_filename], stdout=m_log, stderr=m_log)
+subprocess.Popen(["dieharder", "-d", "12", "-g", "202", "-f", lcg_out_filename], stdout=log_lcg, stderr=log_lcg)
+#subprocess.Popen(["dieharder", "-d", "2", "-g", "202", "-f", mersenne_out_filename], stdout=m_log, stderr=m_log)
 subprocess.Popen(["dieharder", "-d", "3", "-g", "202", "-f", mersenne_out_filename], stdout=m_log, stderr=m_log)
-subprocess.Popen(["dieharder", "-d", "4", "-g", "202", "-f", mersenne_out_filename], stdout=m_log, stderr=m_log)
-subprocess.Popen(["dieharder", "-d", "5", "-g", "202", "-f", mersenne_out_filename], stdout=m_log, stderr=m_log)
-subprocess.Popen(["dieharder", "-d", "6", "-g", "202", "-f", mersenne_out_filename], stdout=m_log, stderr=m_log)
-#subprocess.Popen(["dieharder", "-d", "7", "-g", "202", "-f", mersenne_out_filename], stdout=m_log, stderr=m_log)
 subprocess.Popen(["dieharder", "-d", "14", "-g", "202", "-f", mersenne_out_filename], stdout=m_log, stderr=m_log)
-#subprocess.Popen(["dieharder", "-a", "-g", "202", "-f", mersenne_out_filename], stdout=m_log, stderr=m_log)
-#subprocess.Popen(["dieharder", "-d", "0", "-g", "202", "-f", xoro_out_filename], stdout=x_log, stderr=x_log)
-subprocess.Popen(["dieharder", "-d", "1", "-g", "202", "-f", xoro_out_filename], stdout=x_log, stderr=x_log)
-subprocess.Popen(["dieharder", "-d", "2", "-g", "202", "-f", xoro_out_filename], stdout=x_log, stderr=x_log)
+subprocess.Popen(["dieharder", "-d", "12", "-g", "202", "-f", mersenne_out_filename], stdout=m_log, stderr=m_log)
+#subprocess.Popen(["dieharder", "-d", "2", "-g", "202", "-f", xoro_out_filename], stdout=x_log, stderr=x_log)
 subprocess.Popen(["dieharder", "-d", "3", "-g", "202", "-f", xoro_out_filename], stdout=x_log, stderr=x_log)
-subprocess.Popen(["dieharder", "-d", "4", "-g", "202", "-f", xoro_out_filename], stdout=x_log, stderr=x_log)
-subprocess.Popen(["dieharder", "-d", "5", "-g", "202", "-f", xoro_out_filename], stdout=x_log, stderr=x_log)
-subprocess.Popen(["dieharder", "-d", "6", "-g", "202", "-f", xoro_out_filename], stdout=x_log, stderr=x_log)
 subprocess.Popen(["dieharder", "-d", "14", "-g", "202", "-f", xoro_out_filename], stdout=x_log, stderr=x_log)
+subprocess.Popen(["dieharder", "-d", "12", "-g", "202", "-f", xoro_out_filename], stdout=x_log, stderr=x_log)
+#subprocess.Popen(["dieharder", "-d", "14", "-g", "202", "-f", xoro_out_filename], stdout=x_log, stderr=x_log)
 subprocess.Popen(["ls", "-l"])
